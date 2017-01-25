514b059b097477d96b4465ece50536ed3d0f0667
themattkerr <themattkerr@att.net>
2017-01-24 21:23:34 -0600

    GUI labeling

--- maincontingencywindow.h
+++ maincontingencywindow.h
@@ -2,6 +2,8 @@
 #define MAINCONTINGENCYWINDOW_H
 
 #include <QMainWindow>
+#include <QList>
+#include <QComboBox>
 
 namespace Ui {
 class MainContingencyWindow;
@@ -15,10 +17,20 @@ public:
     explicit MainContingencyWindow(QWidget *parent = 0);
     ~MainContingencyWindow();
 
+private slots:
+    void on_pushButton_Show_5_More_clicked();
+
+    void on_pushButton_Show_5_Fewer_clicked();
+
 private:
     Ui::MainContingencyWindow *ui;
     void loadTitles();
     void setDefaults();
+    void showRows();
+    void setupComboBoxQList();
+    QList <QComboBox*> m_allComboxes;
+    int     m_nRowsToShow;
+
 };
 
 #endif // MAINCONTINGENCYWINDOW_H
